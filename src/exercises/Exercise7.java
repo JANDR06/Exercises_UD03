f7288fdf58b1ed1da01710ab67154902f9371d1b
alejandro jorge <alejorpas2@alu.edu.gva.es>
2023-10-31 16:01:46 +0100

    Commit

--- src/exercises/Exercise7.java
+++ src/exercises/Exercise7.java
@@ -1,4 +1,61 @@
+/*
+7. Create a program with 10 integers entered by the keyboard. The program must
+be able to know how many numbers are positive, negative or cero.
+ */
+
 package exercises;
 
+import java.util.Scanner;
+
 public class Exercise7 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[10];
+
+        Exercise1.fillMatrix(numbers, sc);
+        System.out.println();
+
+        int positives = positivesQuantity(numbers), negatives = negativesQuantity(numbers), zeros = zerosQuantity(numbers);
+        System.out.println("Number of positives: " + positives);
+        System.out.println("Number of negatives: " + negatives);
+        System.out.println("Number of zeros: " + zeros);
+    }
+
+    public static int positivesQuantity(int[] array) {
+        int positives = 0;
+
+        for (int element : array) {
+            if (element > 0) {
+                positives++;
+            }
+        }
+
+        return positives;
+    }
+
+    public static int negativesQuantity(int[] array) {
+        int negatives = 0;
+
+        for (int element : array) {
+            if (element < 0) {
+                negatives++;
+            }
+        }
+
+        return negatives;
+    }
+
+    public static int zerosQuantity(int[] array) {
+        int zeros = 0;
+
+        for (int element : array) {
+            if (element == 0) {
+                zeros++;
+            }
+        }
+
+        return zeros;
+    }
 }
