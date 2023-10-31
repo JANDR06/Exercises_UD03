02c0bf690a118788de31723a0986c27b9c2b9703
alejandro jorge <alejorpas2@alu.edu.gva.es>
2023-10-31 14:49:34 +0100

    Commit

--- src/exercises/Exercise4.java
+++ src/exercises/Exercise4.java
@@ -14,6 +14,55 @@ public class Exercise4 {
         Scanner sc = new Scanner(System.in);
 
         int[] array = new int[Exercise2.sizeArray(sc)];
+        fillArray(array, 0, 200);
+        System.out.println("The maximum value of the array is: " + maxValue(array));
 
+    }
+
+    public static void fillArray(int[] matrix, int lower, int upper) {
+        int primeNumber;
+
+        for (int i = 0; i < matrix.length; i++) {
+            do {
+                primeNumber = Exercise2.generateRandomNumber(lower, upper);
+            } while (!isPrime(primeNumber));
+
+            matrix[i] = primeNumber;
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number <= 3) {
+            return true;
+        }
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int maxValue(int[] array) {
+        int maxValue = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+                maxValue = array[i];
+            }
+
+            if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+
+        return maxValue;
     }
 }
