eae601facc541cbdaad1ebf918d5eb8358cfc5b4
alejandro jorge <alejorpas2@alu.edu.gva.es>
2023-10-31 15:43:43 +0100

    Commit

--- src/exercises/Exercise6.java
+++ src/exercises/Exercise6.java
@@ -1,4 +1,32 @@
+/*
+6. Create a program that calculates the letter of the DNI of any person. To
+calculate the letter you must divide the DNI by 23, and the rest look for it in
+the following list:
+ */
+
 package exercises;
 
+import java.util.Scanner;
+
 public class Exercise6 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char[] letters = {'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
+
+        int position = returnPosition(sc);
+        System.out.println("The letter that corresponds to the DNI is " + letters[position]);
+    }
+
+    public static int returnPosition(Scanner sc) {
+        int dni, position;
+
+        System.out.print("Write your DNI number without the letter: ");
+        dni = sc.nextInt();
+
+        position = dni % 23;
+
+        return position;
+    }
 }
